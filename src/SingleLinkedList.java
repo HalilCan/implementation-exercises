7b43ebd5626a1ab19c9c85ef20746a8b58825420
HalilCan <halilcanmemoglu@gmail.com>
2017-12-31 22:18:10 +0300

    Initial commit

---
+++ src/SingleLinkedList.java
@@ -0,0 +1,71 @@
+public class SingleLinkedList {
+    private SingleNode startNode = null;
+    private SingleNode finalNode = null;
+
+    public SingleLinkedList (SingleNode n) {
+        startNode = n;
+        finalNode = n;
+    }
+
+    public void add (SingleNode n) {
+        finalNode.next = n;
+        finalNode = n;
+    }
+
+    public int size () {
+        int s = 0;
+        if (startNode != null) {
+            s = 1;
+            SingleNode n = startNode;
+            SingleNode current = startNode;
+            while (current != null) {
+                current = SingleNode.next;
+                s++;
+            }
+            return s;
+        } else {
+            return 0;
+        }
+    }
+
+    public SingleNode pop () {
+        SingleNode currentNode = startNode;
+        SingleNode penultimateNode = null;
+
+        while (currentNode != finalNode || currentNode != null) {
+            penultimateNode = currentNode;
+            currentNode = currentNode.next;
+        };
+        if(currentNode == finalNode) {
+            SingleNode tempNode = currentNode.copy();
+            finalNode = penultimateNode;
+            finalNode.next = null;
+            return tempNode;
+        } else {
+            return null;
+        }
+    }
+
+    public SingleNode shift () {
+        SingleNode temp = startNode;
+        startNode = startNode.next;
+        return temp;
+    }
+
+    public SingleNode remove (SingleNode n) {
+        SingleNode currentNode = startNode;
+        SingleNode penultimateNode = null;
+
+        while (currentNode != n || currentNode != null) {
+            penultimateNode = currentNode;
+            currentNode = currentNode.next;
+        };
+        if(currentNode == n) {
+            penultimateNode.next = currentNode;
+            currentNode.next = null;
+            return currentNode;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/SingleNode.java
@@ -0,0 +1,15 @@
+public class SingleNode {
+    public static int info;
+    public static SingleNode next;
+
+    public SingleNode(int i) {
+        info = i;
+        next = null;
+    }
+
+    public SingleNode copy() {
+        SingleNode cpy = new SingleNode(info);
+        cpy.next = next;
+        return cpy;
+    }
+}
